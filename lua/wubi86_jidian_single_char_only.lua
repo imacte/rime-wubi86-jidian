77b51678a1975791a6af39f866191be69c55cad2
yuki <119027683+imacte@users.noreply.github.com>
2026-06-12 22:49:51 +0800

    Update wubi86_jidian_single_char_only.lua

--- lua/wubi86_jidian_single_char_only.lua
+++ lua/wubi86_jidian_single_char_only.lua
@@ -1,7 +1,7 @@
 --- 过滤器：只显示单字
 local function wubi86_jidian_single_char_only(input)
     for cand in input:iter() do
-        if (utf8.len(cand.text) == 1) then
+        if (utf8.len(cand.text) == 1 or cand.type == "date" or cand.type == "time") then
             yield(cand)
         end
     end
